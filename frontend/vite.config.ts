9040332a8f4b227913424ca9389ea3132e5a9c8a
dmitrystas <dmitry.stas@gmail.com>
2026-08-04 13:03:15 +0000

    chore: remove Vite configLoader: 'native' deprecation warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,64 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { readFile } from 'fs/promises'
 // import { readFileSync } from 'fs'
 import { URL, fileURLToPath } from 'node:url'
+import { optimize } from 'svgo'
 import { defineConfig, loadEnv } from 'vite'
+import type { Plugin } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { compileTemplate } from 'vue/compiler-sfc'
 
-import svgLoader from './vite.svg-loader'
+const svgLoader = (): Plugin => ({
+  name: 'vite.svg-loader',
+  enforce: 'pre',
+  async load(id: string) {
+    if (!id.endsWith('.svg?component')) {
+      return
+    }
+
+    const path = id.slice(0, -10),
+      svg = await readFile(path, 'utf-8')
+
+    const svgData = optimize(svg, {
+      path,
+      plugins: [
+        {
+          name: 'preset-default',
+          params: {
+            overrides: {
+              convertPathData: false,
+              mergePaths: false,
+            },
+          },
+        },
+      ],
+    })
+      .data.replace(/<style/g, '<component is="style"')
+      .replace(/<\/style/g, '</component')
+      .replace(/<svg([^>]+)>/, '<svg$1 v-bind="attrs"><slot />')
+
+    const { code } = compileTemplate({
+      id: JSON.stringify(id),
+      source: svgData,
+      filename: path,
+      transformAssetUrls: false,
+    })
+
+    return `
+      ${code}
+      export default (props, { slots, attrs }) => render({
+        attrs: {
+          'aria-hidden': true,
+          ...attrs
+        },
+        $slots: slots
+      }, [])
+    `
+  },
+})
 
 const assets = [
   {
